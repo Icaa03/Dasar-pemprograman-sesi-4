c46e9c63dbffb481896b057a5c99b0e2c3d101ba
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2024-03-15 10:36:39 +0700

    Add files via upload

---
+++ input dua bilangan lebih besar.py
@@ -0,0 +1,8 @@
+bil1 = int(input("masukan bilangan pertama: "))
+bil2 = int(input("masukan bilangan kedua: "))
+
+if bil1 > bil2:
+    print (f"{bil1} lebih besar dari pada {bil2}")
+else:
+    print (f"{bil1} lebih kecil dari pada {bil2}")
+    
